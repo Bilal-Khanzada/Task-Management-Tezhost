d065cb40985e96bd3aad958c37af93ff0fed84fd
Bilal-Khanzada <bilalkzr13@hotmail.com>
2024-05-14 10:15:34 +0500

    vercel deployment

--- backend/Server.js
+++ backend/Server.js
@@ -4,10 +4,18 @@ import connectDB from "./DB/ConnectDb.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./Routes/UserRoutes.js";
 import TaskRoutes from "./Routes/TaskRoutes.js"
+import cors from "cors"
 dotenv.config();
 connectDB();
 const app=express();
+app.use(cors(
+    {origin:['https://deploy-mern-1whq.vercel.app'],
+    methods:["POST","GET"],
+    credentials:true
+}
+))
 app.use(express.json());
+
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 app.use("/api/users",userRoutes);
